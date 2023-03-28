0dae5d747b2b7229db3682dd5c543c3b17ef44fa
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-03-28 01:09:35 -0400

    improved check centroids

--- Q2/check_centroids.py
+++ Q2/check_centroids.py
@@ -3,15 +3,11 @@
 import sys
 
 # This scripts is checking for convergence or to check if the centroids are no longer changing
-old = sys.argv[1]
-old = [round(float(v),2) for v in old.strip().split(',')]
-new = sys.argv[2]
-new = [round(float(v),2) for v in new.strip().split(',')]
+old, new = sys.argv[1:]
+old = [round(float(val), 2) for val in old.split(',')]
+new = [round(float(val), 2) for val in new.split(',')]
 
-equal = 0
-for i in range(len(old)):
-    if old[i] == new[i]:
-        equal += 1
+equal = sum(1 for x, y in zip(old, new) if x == y) # Zip iterates over both lists simultaneously 
 
 # 3 because we have 3 dimensions for the centroids from our original data
 print(1 if equal == 3 else 0)
